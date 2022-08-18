f80c46de71a75eb69a91f809cd187c150dd9d8ad
Theboral <110025150+Theboral-Ayyanar@users.noreply.github.com>
2022-08-18 17:15:43 +0530

    Add files via upload

---
+++ Asha.java
@@ -0,0 +1,31 @@
+package condition;
+
+import java.util.Scanner;
+
+public class Asha {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter your age:");
+		
+		int age = sc.nextInt();
+		
+		if(age > 18)
+		{
+			
+		System.out.print("Your age is eligible for voting");
+		}
+		else
+		{
+		System.out.println("Not eligible for voting");
+		}
+
+		
+				
+				
+
+	}
+
+}
+                                                                                                                                                                                                                                                        
